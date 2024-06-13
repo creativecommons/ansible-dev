ac06af86d858ff64834e72a2fcd13d298da99970
Shafiya Heena <shafiya@creativecommons.org>
2024-06-13 10:36:07 -0400

    update generate script

--- generate_ssh_keys.sh
+++ generate_ssh_keys.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 # Define key parameters
-KEY_PATH="./sysadmin-ssh-keys"
-KEY_FILENAME="rsa_sysadmin"
+KEY_PATH='./sysadmin-ssh-keys'
+KEY_FILENAME='rsa_sysadmin'
 
 # Create a directory to store the SSH keys if it doesn't exist
 mkdir -p ${KEY_PATH}
 
 # Generate SSH key pair only if they don't already exist
-if [ ! -f ${KEY_PATH}/${KEY_FILENAME} ]; then
+if [ ! -f ${KEY_PATH}/${KEY_FILENAME} ]
+then
     ssh-keygen -b 4096 -t rsa -C "sysadmin" -f ${KEY_PATH}/${KEY_FILENAME} -N ''
     echo "SSH keys generated and stored in ${KEY_PATH}/${KEY_FILENAME}"
 else
